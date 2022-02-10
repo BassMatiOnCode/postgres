219aa767baa853510e6c17ddb6f99cbf17668b5f
BassmatiOnMusic <bassmatiOnMusic@freenet.de>
2022-02-10 17:12:27 +0100

    Files moved to docs folder

--- start-dev.bat
+++ start-dev.bat
@@ -1,5 +1,6 @@
 net start postgresql-x64-13
+cd docs
 start /min deno-file-server
-start /min cmd.exe /k "cd /d d:\github\postgtes&psql"
+start /min cmd.exe /k "cd /d d:\github\postgtes\docs&psql"
 d:\progs\VS2022\Common7\IDE\devenv.com "D:\github\postgres\"
 start "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "http://localhost:4507/data-types.htm"
