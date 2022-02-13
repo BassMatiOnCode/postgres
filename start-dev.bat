92bdd4968fc9cde969eea473094b93ff6e2a8de7
BassmatiOnMusic <bassmatiOnMusic@freenet.de>
2022-02-13 15:31:49 +0100

    Should work now

--- start-dev.bat
+++ start-dev.bat
@@ -1,6 +1,18 @@
-net start postgresql-x64-13
+echo on
+cd /d d:\github\bassmati-on-code\postgres\
+pause
+chcp 1252
+@SET PGDATA=D:\Data\postgres
+@SET PGDATABASE=postgres
+@SET PGUSER=postgres
+@SET PGPORT=5432
+@SET PGLOCALEDIR=D:\progs\pg\14\share\locale
+pause
+runas /user:administrator "net start postgresql-x64-14"
+pause
+start /min "cmd.exe /k d:\progs\pg\14\bin\psql.exe"
+pause
 cd docs
 start /min deno-file-server
-start /min cmd.exe /k "cd /d d:\github\bassmati-on-code\postgtes\docs&psql"
 d:\progs\VS2022\Common7\IDE\devenv.com "D:\github\bassmati-on-code\postgres\"
-start "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "http://localhost:4507/data-types.htm"
+start "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "http://localhost:4507/psql.htm"
