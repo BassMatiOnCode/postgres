09fbb716ada6fcf76ce6cc4742cd20f5fdb9abe3
BassmatiOnMusic <bassmatiOnMusic@freenet.de>
2022-02-13 15:29:14 +0100

    Added

--- start-dev.bat
+++ start-dev.bat
@@ -1,6 +1,6 @@
 net start postgresql-x64-13
 cd docs
 start /min deno-file-server
-start /min cmd.exe /k "cd /d d:\github\postgtes\docs&psql"
-d:\progs\VS2022\Common7\IDE\devenv.com "D:\github\postgres\"
+start /min cmd.exe /k "cd /d d:\github\bassmati-on-code\postgtes\docs&psql"
+d:\progs\VS2022\Common7\IDE\devenv.com "D:\github\bassmati-on-code\postgres\"
 start "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "http://localhost:4507/data-types.htm"
